8836407beda5f3657268cd8c3700a0ae3075106b
abelski <abelski@tut.by>
2025-03-06 13:55:48 +0200

    added requirments.txt and template env

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import os
 from typing import final
-from  telegram import Update
-from  telegram.ext import Application, CommandHandler, MessageHandler, filters,ContextTypes
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN: final = os.getenv("TOKEN")
 USERNAME: final = os.getenv("USERNAME")
 
+
 # Commands
 async def start_command(update: Update, context: ContextTypes):
     await update.message.reply_text("Hello! I'm a bot for freedive coaching.")
 
+
 async def help_command(update: Update, context: ContextTypes):
     await update.message.reply_text("help")
+
 
 async def custom_command(update: Update, context: ContextTypes):
     await update.message.reply_text("custom")
 
-#Responceses
+
+# Responceses
 
 def handle_response(text: str) -> str:
     processsed_text = text.lower()
@@ -26,6 +30,8 @@ def handle_response(text: str) -> str:
         return "Hello! I'm a bot for freedive coaching?"
 
     return "I don't understand you"
+
+
 async def handle_message(update: Update, context: ContextTypes):
     message_type = update.message.chat.type
     print(f'message_type: {message_type}')
@@ -35,25 +41,26 @@ async def handle_message(update: Update, context: ContextTypes):
     else:
         await update.message.reply_text("I can only talk in private chats")
 
+
 async def error(update: Update, context: ContextTypes):
     await update.message.reply_text("An error occured")
+
 
 # Main runner
 if __name__ == "__main__":
     app = Application.builder().token(TOKEN).build()
 
-    #COMMANDS (need to be added first in bot father)
+    # COMMANDS (need to be added first in bot father)
     app.add_handler(CommandHandler("start", start_command))
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(CommandHandler("custom", custom_command))
 
-    #MESSAGES
+    # MESSAGES
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
 
-    #ERRORS
+    # ERRORS
     app.add_error_handler(error)
 
-
-    #CHECK messages every 5 seconds
+    # CHECK messages every 5 seconds
     print("polling...")
     app.run_polling(poll_interval=5)
